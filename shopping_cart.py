98d1e00bdb8d2703aa13bafeb5b927706e119dd9
Samuel Gómez Balderas <samuel.gomez.balderas@gmail.com>
2023-03-23 23:35:44 -0600

    3-6: Métodos assertTrue y assertFalse

--- shopping_cart.py
+++ shopping_cart.py
@@ -1,3 +1,16 @@
+from typing import List
+from product import Product
 
 class ShoppingCart:
-    pass
+
+    def __init__(self) -> None:
+        self.products: List[Product] = []
+
+    def add_product(self, product: Product) -> None:
+        self.products.append(product)
+
+    def is_empty(self) -> bool:
+        return len(self.products) == 0
+
+    def has_products(self) -> bool:
+        return not self.is_empty()
