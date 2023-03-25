4e50d4a0c730e6d8b398c76c92e15a0268dc1144
Samuel Gómez Balderas <samuel.gomez.balderas@gmail.com>
2023-03-24 22:17:10 -0600

    3-8: Método assertRises

--- product.py
+++ product.py
@@ -1,7 +1,14 @@
 
+class ProductDiscountError(Exception):
+    pass
+
 class Product:
     
     def __init__(self, name: str, price: float, discount: float = 0.0) -> None:
         self.name = name
         self.price = price
+
+        if discount > price:
+            raise ProductDiscountError('Descuento mayor que precio.')
+
         self.discount = discount
