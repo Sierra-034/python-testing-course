806606ecc27e0dfe3c2017278e3daf5eabeccd7f
Samuel Gómez Balderas <samuel.gomez.balderas@gmail.com>
2023-03-24 22:17:10 -0600

    3-8: Método assertRises

--- test_shopping_cart.py
+++ test_shopping_cart.py
@@ -1,6 +1,6 @@
 import unittest
 
-from product import Product
+from product import Product, ProductDiscountError
 from shopping_cart import ShoppingCart
 
 class TestShoppingCart(unittest.TestCase):
@@ -56,7 +56,10 @@ class TestShoppingCart(unittest.TestCase):
     def test_product_notin_shopping_cart(self):
         self.shopping_cart_2.remove_product(self.smartphone)
         self.assertNotIn(self.smartphone, self.shopping_cart_2.products)
-        
+
+    def test_discount_error(self):
+        with self.assertRaises(ProductDiscountError):
+            Product(name='Example', price=10.0, discount=11.0)
 
 if __name__ == '__main__':
     unittest.main()
