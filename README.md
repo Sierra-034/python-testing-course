508e5ed49624c5edf591444bb6b32e8f55bb574e
Samuel Gómez <samuel.gomez.balderas@gmail.com>
2024-09-09 17:04:11 -0600

    4-3: Ejecutar pruebas unitarias

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 - First you need to install this package locally: `pip install -e .`
 - To run unittest tests, you need: `python -m unittest discover -s test_unittest`
 - To run pytest tests, you first need pytest installed: `pip install pytest`, then: `pytest test_pytest`
+
+- You can also run individual or set of test: `pytest test_pytest/test_main.py::TestExample::test_suma_dos_numeros`
--- test_pytest/test_main.py
+++ test_pytest/test_main.py
@@ -1,8 +1,16 @@
 import pytest
 
+def test_isolated():
+    assert True == True, 'True is not True'
+
 class TestExample:
     def test_suma_dos_numeros(self):
         assert 10 + 10 == 20, 'Suma incorrecta'
 
     def test_resta_dos_numeros(self):
         assert 10 - 10 == 0, 'Resta incorrecta'
+
+
+class TestExample2:
+    def test_multiplicacion_dos_numeros(self):
+        assert 10 * 10 == 100, 'Multiplicacion incorrecta'
