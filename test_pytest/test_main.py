ec6bbc76938cba513945f554f51ae6f1428ca8c1
Samuel Gómez <samuel.gomez.balderas@gmail.com>
2024-09-09 16:39:49 -0600

    Clase pytest

--- test_pytest/test_main.py
+++ test_pytest/test_main.py
@@ -1,4 +1,8 @@
 import pytest
 
-def test_example():
-    assert 10 == 10
+class TestExample:
+    def test_suma_dos_numeros(self):
+        assert 10 + 10 == 20, 'Suma incorrecta'
+
+    def test_resta_dos_numeros(self):
+        assert 10 - 10 == 0, 'Resta incorrecta'
