d6a4059dea46b1722ec937b1451f254b5763d868
Samuel Gómez <samuel.gomez.balderas@gmail.com>
2024-09-10 14:07:27 -0600

    4-4: SetUp and TearDown

--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 - To run pytest tests, you first need pytest installed: `pip install pytest`, then: `pytest test_pytest`
 
 - You can also run individual or set of test: `pytest test_pytest/test_main.py::TestExample::test_suma_dos_numeros`
+- To see standard output you'll need to set `-s`: `pytest -s <path_to_test>`
--- test_pytest/test_main.py
+++ test_pytest/test_main.py
@@ -4,11 +4,27 @@ def test_isolated():
     assert True == True, 'True is not True'
 
 class TestExample:
+
+    @classmethod
+    def setup_class(cls):
+        print('>>> setup_class executes once before all methods')
+
+    @classmethod
+    def teardown_class(cls):
+        print('>>> teardownd_class executes once after all methods')
+    
+    def setup_method(self):
+        self.numero_uno = 10
+        self.numero_dos = 20
+
+    def teardown_method(self):
+        print('>>> teardown_method executing')
+    
     def test_suma_dos_numeros(self):
-        assert 10 + 10 == 20, 'Suma incorrecta'
+        assert self.numero_uno + self.numero_dos == 30, 'Suma incorrecta'
 
     def test_resta_dos_numeros(self):
-        assert 10 - 10 == 0, 'Resta incorrecta'
+        assert self.numero_uno - self.numero_dos == -10, 'Resta incorrecta'
 
 
 class TestExample2:
