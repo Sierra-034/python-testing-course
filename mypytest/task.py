839e3998930085021190e99a4921cbe3380a9b2d
Samuel Gómez <samuel.gomez.balderas@gmail.com>
2024-09-11 12:16:36 -0600

    4-7: Manejo de errores

--- mypytest/task.py
+++ mypytest/task.py
@@ -1,7 +1,15 @@
+from datetime import datetime
+
+class DueDateError(Exception):
+    pass
 
 class Task:
     def __init__(self, title, description, assigned_to, due_date):
         self.title = title
         self.description = description
         self.assigned_to = assigned_to
+
+        if due_date < datetime.now():
+            raise DueDateError('Invalid due date')
+        
         self.due_date = due_date
--- test_pytest/test_task.py
+++ test_pytest/test_task.py
@@ -1,17 +1,22 @@
 import pytest
-from datetime import datetime
+from datetime import datetime, timedelta
 
-from mypytest import Task
+from mypytest import Task, DueDateError
 
 class TestTask:
     def test_task(self):
         assert True
     
     def test_new_task(self):
-        due_date = datetime.now()
+        due_date = datetime.now() + timedelta(days=1)
         task = Task('Title', 'Description', 'samuel_g', due_date)
 
         assert task.title == 'Title'
         assert task.description == 'Description'
         assert task.assigned_to == 'samuel_g'
         assert task.due_date == due_date
+
+    def test_due_date_error(self):
+        with pytest.raises(DueDateError):
+            due_date = datetime.now() - timedelta(days=1)
+            task = Task('Title', 'Description', 'samuel_g', due_date)
